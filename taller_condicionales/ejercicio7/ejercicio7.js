d1c0a5fc8a5cb7d7a5b9d4c1a980374e3fd781df
Elver Tunubala <elvertunubala@gmail.com>
2020-12-02 18:14:32 -0500

    ejercicio 7

--- taller_condicionales/ejercicio7/ejercicio7.js
+++ taller_condicionales/ejercicio7/ejercicio7.js
@@ -1,5 +1,30 @@
-alert("hola clase");
 let numUno;
 let numDos;
 let numTres;
+
+function Calcular(){
+    numUno = Number(document.getElementById('NumUno').value);
+    numDos = Number(document.getElementById('NumDos').value);
+    numTres = Number(document.getElementById('NumTres').value);
+    numMayor(numUno,numDos,numTres)
+}
+function numMayor(num1,num2,num3){
+    if(num1>num2){
+        if(num1>num3){
+            alert('El numero mayor es:'+ " "+num1);
+        }
+        else{
+    numUno = Number(document.getElementById('NumUno').value)
+          alert('El numero mayor es:'+ " "+num3);
+        }
+    }
+    else{
+        if(num1>num3){
+            alert('El numero mayor es:'+" "+ num2);
+        }
+        else{
+            alert('El numero mayor es:'+" "+ num3);
+        }
+    }
+}
 
